855064c916487cf57f90e729fe65f73b08073795
danielnaoexiste <danieldenardo1@gmail.com>
2024-05-25 23:41:38 -0300

    fix: convert tech badge images to webp

--- src/components/TechBadges/TechBadges.tsx
+++ src/components/TechBadges/TechBadges.tsx
@@ -4,84 +4,84 @@ export const TechBadges = () => (
   <div className="flex flex-wrap my-4 gap-x-4 gap-y-3">
     <Image
       alt="React"
-      src="/badges/react.svg"
+      src="/badges/react.webp"
       className="h-6 rounded-md"
       height="24"
       width="73"
     />
     <Image
       alt="React Native"
-      src="/badges/react-native.svg"
+      src="/badges/react-native.webp"
       className="h-6 rounded-md"
       height="24"
       width="121"
     />
     <Image
       alt="Next"
-      src="/badges/next.svg"
+      src="/badges/next.webp"
       className="h-6 rounded-md"
       height="24"
       width="67"
     />
     <Image
       alt="Vue"
-      src="/badges/vue.svg"
+      src="/badges/vue.webp"
       className="h-6 rounded-md"
       height="24"
       width="76"
     />
     <Image
       alt="Typescript"
-      src="/badges/typescript.svg"
+      src="/badges/typescript.webp"
       className="h-6 rounded-md"
       height="24"
       width="108"
     />
     <Image
       alt="Javascript"
-      src="/badges/javascript.svg"
+      src="/badges/javascript.webp"
       className="h-6 rounded-md"
       height="24"
       width="108"
     />
     <Image
       alt="Node"
-      src="/badges/node.svg"
+      src="/badges/node.webp"
       className="h-6 rounded-md"
       height="24"
       width="86"
     />
     <Image
       alt="Nest"
-      src="/badges/nest.svg"
+      src="/badges/nest.webp"
       className="h-6 rounded-md"
       height="24"
       width="67"
     />
     <Image
       alt="Git"
-      src="/badges/git.svg"
+      src="/badges/git.webp"
       className="h-6 rounded-md"
       height="24"
       width="58"
     />
     <Image
       alt="HTML"
-      src="/badges/html.svg"
+      src="/badges/html.webp"
       className="h-6 rounded-md"
       height="24"
       width="76"
     />
     <Image
       alt="CSS"
-      src="/badges/css.svg"
+      src="/badges/css.webp"
       className="h-6 rounded-md"
       height="24"
       width="66"
     />
     <Image
       alt="MongoDB"
-      src="/badges/mongodb.svg"
+      src="/badges/mongodb.webp"
       className="h-6 rounded-md"
       height="24"
       width="95"
