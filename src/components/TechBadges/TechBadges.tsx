9e96967070a8348180d8eaff3123b970c04e7a2c
danielnaoexiste <danieldenardo1@gmail.com>
2024-05-25 23:28:39 -0300

    fix: add tech stack image badges locally

--- src/components/TechBadges/TechBadges.tsx
+++ src/components/TechBadges/TechBadges.tsx
@@ -4,84 +4,84 @@ export const TechBadges = () => (
   <div className="flex flex-wrap my-4 gap-x-4 gap-y-3">
     <Image
       alt="React"
-      src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB"
+      src="/badges/react.svg"
       className="h-6 rounded-md"
       height="24"
       width="73"
     />
     <Image
       alt="React Native"
-      src="https://img.shields.io/badge/react_native-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
+      src="/badges/react-native.svg"
       className="h-6 rounded-md"
       height="24"
       width="121"
     />
     <Image
       alt="Next"
-      src="https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white"
+      src="/badges/next.svg"
       className="h-6 rounded-md"
       height="24"
       width="67"
     />
     <Image
       alt="Vue"
-      src="https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D"
+      src="/badges/vue.svg"
       className="h-6 rounded-md"
       height="24"
       width="76"
     />
     <Image
       alt="Typescript"
-      src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"
+      src="/badges/typescript.svg"
       className="h-6 rounded-md"
       height="24"
       width="108"
     />
     <Image
       alt="Javascript"
-      src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black"
+      src="/badges/javascript.svg"
       className="h-6 rounded-md"
       height="24"
       width="108"
     />
     <Image
       alt="Node"
-      src="https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white"
+      src="/badges/node.svg"
       className="h-6 rounded-md"
       height="24"
       width="86"
     />
     <Image
       alt="Nest"
-      src="https://img.shields.io/badge/Nest-EA286C?style=for-the-badge&logo=nestjs&logoColor=white"
+      src="/badges/nest.svg"
       className="h-6 rounded-md"
       height="24"
       width="67"
     />
     <Image
       alt="Git"
-      src="https://img.shields.io/badge/GIT-E44C30?style=for-the-badge&logo=git&logoColor=white"
+      src="/badges/git.svg"
       className="h-6 rounded-md"
       height="24"
       width="58"
     />
     <Image
       alt="HTML"
-      src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white"
+      src="/badges/html.svg"
       className="h-6 rounded-md"
       height="24"
       width="76"
     />
     <Image
       alt="CSS"
-      src="https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white"
+      src="/badges/css.svg"
       className="h-6 rounded-md"
       height="24"
       width="66"
     />
     <Image
       alt="MongoDB"
-      src="https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white"
+      src="/badges/mongodb.svg"
       className="h-6 rounded-md"
       height="24"
       width="95"
